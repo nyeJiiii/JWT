cc3c6d0247135c6970ac9469c92c98fb719094fa
HanHyeJi <hjhan.0621@gmail.com>
2023-09-19 16:09:55 +0900

    ** feat: JWT 토큰 서버 구축 완료 (오류: UserDetailsService returned null)

--- src/main/java/com/cos/jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Date;
 
 // 스크링 시큐리티의 아래 필터 상속
 // /login 요청해서 Username, password 전송(post)시
@@ -26,6 +29,11 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     private final AuthenticationManager authenticationManager;
 
     // /login 요청하면 로그인 시도를 위해 실행되는 함수
+
+    // attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행
+    // JWT 토큰을 만들어서 request 요청한 사용자에게 JWT 토큰을 response 해주면 됨
+
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         System.out.println("JWTAuthenticationFilter: 로그인 시도중");
@@ -59,8 +67,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                     authenticationManager.authenticate(authenticationToken);
 
             // authentication 객체가 sesion 영역에 저장됨 => 로그인이 되었다는 뜻
-            PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
-            System.out.println("로그인 완료됨: " + principalDetails.getUser().getUsername());
+//            System.out.println("로그인 완료됨: " + principalDetails.getUser().getUsername());
             System.out.println("1===========================");
             return authentication;
         } catch (IOException e) {
@@ -69,14 +76,23 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         System.out.println("2===========================");
 
         return null;
+    }
 
-        // attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행
-        // JWT 토큰을 만들어서 request 요청한 사용자에게 JWT 토큰을 response 해주면 됨
-        @Override
-        protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-            System.out.println("successfulAuthentication 실행 (인증 완료됨)");
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("successfulAuthentication 실행 (인증 완료됨)");
+        PrincipalDetails principalDetails = (PrincipalDetails)authResult.getPrincipal();
 
-            super.successfulAuthentication(request, response, chain, authResult);
-        }
+        // RSA 방식X, hash 암호방식
+        String jwtToken = JWT.create()
+                .withSubject("cos토큰")
+                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512("cos"));
+
+        response.addHeader("Authorization", "Bearer" + jwtToken);
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
